1a7003d5f29dc1f8631209f748e1179f83bd8ae2
m.abdo <nehhuby.dev2>
2022-11-22 17:45:37 +0200

    first commit

---
+++ database/migrations/2022_11_22_133337_create_pictures_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('path',100);
+            $table->unsignedBigInteger('album_id');
+            $table->foreign('album_id')->references('id')->on('albums');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictures');
+    }
+};
---
+++ resources/views/Layout/nav.blade.php
@@ -0,0 +1,4 @@
+<nav class="flex items-top justify-center bg-gray-100">
+    <a href="#">Albums</a>
+    <a href="#">All Pictures</a>
+</nav>
